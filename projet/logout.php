4ca74656873b24e08fed7b304c36e4c64c5ded9c
Elcroko <92429672+Elcroko@users.noreply.github.com>
2025-03-24 20:04:57 +0100

    Add files via upload

---
+++ projet/logout.php
@@ -0,0 +1,9 @@
+<?php
+session_start();        // On démarre la session
+session_unset();        // Supprime toutes les variables de session
+session_destroy();      // Détruit la session
+
+// Redirection vers la page d'accueil
+header("Location: index.php");
+exit;
+?>
